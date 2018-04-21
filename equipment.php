b6f9f3302596656d6949b2ed1312c58d84df21b4
Oliver Chilton <you@example.com>
2018-04-21 14:35:59 +0100

    20180421v1

--- equipment.php
+++ equipment.php
@@ -7,38 +7,45 @@
 	<div class="main-wrapper">
 		<h2>Equipment Faults</h2>
 		<?php
-			$sql="SELECT equipment_id FROM equipment";
-			$result=mysqli_query($conn, $sql);
+			if (isset($_SESSION['u_uid'])){
+				$sql="SELECT equipment_id FROM equipment";
+				$result=mysqli_query($conn, $sql);
 
-			echo "<form class='equipmentfault-form' action='includes/equipment.inc.php' method='POST'>";
-			echo "<select id='equipment' name='equipment'>
-					<option value='0'>Select Equipment</option>";
-			while ($row = mysqli_fetch_array($result)){
-				echo "<option value'" . $row['equipment_id'] ."'>" . $row['equipment_id'] ."</option>";
+				echo "<form class='equipmentfault-form' action='includes/equipment.inc.php' method='POST'>";
+				echo "<select id='equipment' name='equipment'>
+						<option value='0'>Select Equipment</option>";
+				while ($row = mysqli_fetch_array($result)){
+					echo "<option value'" . $row['equipment_id'] ."'>" . $row['equipment_id'] ."</option>";
+				}
+				echo "</select>";
 			}
-
-			echo "</select>";
 		?>
-
-		<select id='severity' name='severity'>
-			<option value='0'>Select Severity of Fault</option>
-			<option value='1'>- Not Fundamental to Operation of Studio</option>
-			<option value='2'>- Fundamental to Operation of Studio</option>
-		</select>
-		<select id='quantity' name='quantity'>
-			<option value='0'>Select Quantity of Faulty Equipment</option>
-			<option value='1'>- 1</option>
-			<option value='2'>- 2</option>
-			<option value='3'>- 3</option>
-			<option value='4'>- 4</option>
-			<option value='5'>- 5</option>
-			<option value='6'>- 6</option>
-			<option value='7'>- 7</option>
-			<option value='8'>- 8</option>
-			<option value='9'>- 9</option>
-			<option value='10'>- 10</option>
-		</select>
-		<button type='submit' name ='submit'>Report Fault</button>
+		<?php
+			if (isset($_SESSION['u_uid'])){
+				echo "
+				<select id='severity' name='severity'>
+					<option value='0'>Select Severity of Fault</option>
+					<option value='1'>- Not Fundamental to Operation of Studio</option>
+					<option value='2'>- Fundamental to Operation of Studio</option>
+				</select>
+				<select id='quantity' name='quantity'>
+					<option value='0'>Select Quantity of Faulty Equipment</option>
+					<option value='1'>- 1</option>
+					<option value='2'>- 2</option>
+					<option value='3'>- 3</option>
+					<option value='4'>- 4</option>
+					<option value='5'>- 5</option>
+					<option value='6'>- 6</option>
+					<option value='7'>- 7</option>
+					<option value='8'>- 8</option>
+					<option value='9'>- 9</option>
+					<option value='10'>- 10</option>
+				</select>
+				<button type='submit' name ='submit'>Report Fault</button>";
+			}else{
+				echo "<h3>You must be logged in to report a fault.</h3>";
+			}
+		?>
 	</div>
 </section>
 
