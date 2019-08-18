1fe95cad88ee8afaf44c59925c135088cbdc9d38
Oliver Chilton <you@example.com>
2019-08-18 13:53:45 +0100

    18082019

--- python/Conn Test.py
+++ python/Conn Test.py
@@ -11,18 +11,3 @@ currentTime = '%H:%M:%S'
 stime2 = time.localtime()
 gtime = time.strftime(currentTime, stime2)
 print(gtime)
-
-connection = pymysql.connect(host='localhost', user='root', passwd='', db='studiobookingsystem')
-
-rfidIn = str(input("Please scan your card/fob: "))
-
-
-try:
-  with connection.cursor() as cursor:
-    sql = "SELECT user_uid FROM users WHERE user_rfid = &s";
-    cursor.execute(sql, (rfidIn, ))
-    result = cursor.fetchall()
-    print(result)
-
-finally:
-  connection.close()
